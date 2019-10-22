865b3eeaf630d6c356b0bfcb7b1dd697bfeb5cf6
Carl-Stanley <cds.beml@gmail.com>
2019-10-21 18:30:52 -0700

    Commit

--- calendar/src/App.tsx
+++ calendar/src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { ScheduleComponent } from '@syncfusion/ej2-react-schedule';
 import { Inject, Month,ViewDirective, ViewsDirective} from '@syncfusion/ej2-react-schedule';
 
 class App extends React.Component {
-
+  
+    
   public render() {
     
     return <ScheduleComponent currentView='Month'>
